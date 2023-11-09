3ebda6ac2c3e0a31b9736cdf3bcaeb485a02ddb5
astavrop0 <andr.stavrop@gmail.com>
2023-11-09 15:12:19 +0100

    initialized project structure

---
+++ docs/index.rst
@@ -0,0 +1,24 @@
+.. MAI_for_stock_return_predictability documentation master file, created by
+   sphinx-quickstart.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+MAI_for_stock_return_predictability documentation!
+==============================================
+
+Contents:
+
+.. toctree::
+   :maxdepth: 2
+
+   getting-started
+   commands
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='In this project we are examining whether the macroeconomic attention indices proposed by Fischer et al. (2022) have the ability to predict stock returns based on some statistical and machine learning algorithms. To evaluate that we compare their predictability efficiency with that of other popular macroeconomic variables.',
+    author='Andreas Stavropoulos',
+    license='MIT',
+)
